dd94bc1bd9c5588ba9418ad3d1381582d9ebd67a
walnutmonster <filipbrakl@gmail.com>
2025-12-08 18:09:46 +0100

    enable format on save for rust

--- init.lua
+++ init.lua
@@ -687,6 +687,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        rust = { 'rustfmt' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
