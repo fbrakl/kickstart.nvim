28a7e7d7197a2aa110f32255f49a7ecec514b5db
walnutmonster <filipbrakl@gmail.com>
2026-01-16 20:48:44 +0100

    enable ty and ruff, turn off ruff error detection

--- init.lua
+++ init.lua
@@ -607,6 +607,14 @@ require('lazy').setup({
         -- gopls = {},
         -- pyright = {},
         rust_analyzer = {},
+        ty = {},
+        ruff = {
+          init_options = {
+            settings = {
+              showSyntaxErrors = false,
+            },
+          },
+        },
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
